e5ffe1bb733e7061c9513736561f48072ca7a1df
claudiaps <clp.sampedro@gmail.com>
2024-03-12 19:55:02 -0300

    refactor(pares.js): improove variable name and console.log

--- pares.js
+++ pares.js
@@ -1,5 +1,8 @@
 const array = [2, 4, 3, 1, 6, 7, 9, 10, 18, 7, 9, 22];
 
-const pares = array.filter((item) => item % 2 === 0);
+const numerosPares = array.filter((item) => item % 2 === 0);
 
-console.log("Os números pares são: ", pares);
+console.log(
+  "Os números pares do array [2, 4, 3, 1, 6, 7, 9, 10, 18, 7, 9, 22] são: ",
+  numerosPares
+);
