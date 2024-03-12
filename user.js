d6bb9d4b5de461a9ac72c6d37be1e5043a6e555a
claudiaps <clp.sampedro@gmail.com>
2024-03-12 20:32:15 -0300

    feat: create user

---
+++ user.js
@@ -0,0 +1,17 @@
+const user = {
+  name: "Claudia",
+  city: "Campo Mourão",
+  work: "developer",
+  hobbie: "play games",
+};
+
+console.log(
+  "Nome: ",
+  user.name,
+  "Cidade: ",
+  user.city,
+  "Work: ",
+  user.work,
+  "Hobbie: ",
+  user.hobbie
+);
