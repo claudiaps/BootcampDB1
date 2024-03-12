20e1ab0afa45b93817e691be73b8f68c4c671dd6
Cláudia Poiet <clp.sampedro@gmail.com>
2024-03-11 22:24:21 -0300

    Update pares.js

--- pares.js
+++ pares.js
@@ -1,5 +1,5 @@
-const array = [2, 4, 3, 1, 6, 7, 9, 10];
+const array = [2, 4, 3, 1, 6, 7, 9, 10, 18, 7, 9, 22];
 
 const pares = array.filter((item) => item % 2 === 0);
 
-console.log("Números pares: ", pares);
+console.log("Os números pares são: ", pares);
