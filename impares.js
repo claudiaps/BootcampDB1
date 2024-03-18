7282ab8648db700cc31f9789c4e9ba44bad712bc
Cláudia Poiet <clp.sampedro@gmail.com>
2024-03-18 20:31:15 -0300

    Update impares.js

--- impares.js
+++ impares.js
@@ -1,5 +1,5 @@
-const array = [2, 4, 3, 1, 6, 7, 9, 10, 90, 8, 7, 5];
+const array = [2, 4, 3, 1, 6, 7, 9, 10, 90, 8, 7, 5, 9, 1, 20, 3, 67];
 
 const impares = array.filter((item) => item % 2 !== 0);
 
-console.log("Números ímpares: ", impares);
+console.log("Os números ímpares são: ", impares);
