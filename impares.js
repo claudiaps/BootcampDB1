3d12dc14a43e39084d0603eef95d399f39c910ad
claudiaps <clp.sampedro@gmail.com>
2024-03-18 20:32:33 -0300

    feat: editando arquivo impares

--- impares.js
+++ impares.js
@@ -1,5 +1,5 @@
-const array = [2, 4, 3, 1, 6, 7, 9, 10, 90, 8, 7, 5];
+const array = [2, 6, 7, 9, 10, 90, 8, 7, 5, 7, 6, 4];
 
 const impares = array.filter((item) => item % 2 !== 0);
 
-console.log("Números ímpares: ", impares);
+console.log("Esses são os números ímpares: ", impares);
