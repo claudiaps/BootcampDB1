7332b470b08d9918aa973fea9f1929eb048997d6
claudiaps <clp.sampedro@gmail.com>
2024-03-18 21:35:17 -0300

    mudanca aquivo pares.js

--- pares.js
+++ pares.js
@@ -1,8 +1,5 @@
-const array = [2, 4, 3, 1, 6, 7, 9, 10, 18, 7, 9, 22];
+const array = [2, 4, 3];
 
 const numerosPares = array.filter((item) => item % 2 === 0);
 
-console.log(
-  "Os números pares do array [2, 4, 3, 1, 6, 7, 9, 10, 18, 7, 9, 22] são: ",
-  numerosPares
-);
+console.log("Os números pares do array [2, 4, 3] são: ", numerosPares);
