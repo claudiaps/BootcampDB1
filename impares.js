c9e4626e2fe9431c40d55fbae1ba53d22c9ef8c6
claudiaps <clp.sampedro@gmail.com>
2024-03-18 21:34:04 -0300

    mudanca impares.js

--- impares.js
+++ impares.js
@@ -1,5 +1,5 @@
-const array = [2, 4, 3, 1, 6, 7, 9, 10, 90, 8, 7, 5, 9, 1, 20, 3, 67];
+const array = [2, 4, 3, 1, 6, 7, 9, 10, 90, 8, 7, 5, 9];
 
 const impares = array.filter((item) => item % 2 !== 0);
 
-console.log("Os números ímpares são: ", impares);
+console.log("Os números ímpares são (editado na branch CherryPick): ", impares);
